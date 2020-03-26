89184b9753979ea8da76879a8269c6a481bcdec1
Joel <joel.m.maenpaa@gmail.com>
2020-03-26 17:13:13 +0200

    job schema

---
+++ models/Job.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const now = new Date().now
+
+const JobSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: Array,
+        required: true
+    },
+    salary: {
+        type: Schema.Types.Decimal128,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: now
+    },
+    // ownerID commented for now since we don't have any users yet
+    /*ownerID: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }*/
+})
+
+module.exports = mongoose.model('Job', JobSchema, 'jobs')
